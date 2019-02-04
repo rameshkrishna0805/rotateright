a7a038a0501501a0b48802ecaee71d72d9f361b0
rameshkrishna0805 <vijayarameshkrishna@gmail.com>
2019-02-04 14:09:47 -0800

    sample

--- rotateright/Source.c
+++ rotateright/Source.c
@@ -1,25 +1,35 @@
 #include<stdio.h>
+#include<math.h>
 int main()
 {
-	int a[10];
+	
 	int n;
 	int i = 0;
 	int p;
 	int count = 0;
 	scanf_s("%d", &n);
 	scanf_s("%d", &p);
+	i = n;
 	while (n > 0)
 	{
 		count++;
-		a[i] = n % 10;
 		n = n / 10;
-		i++;
 	}
-	for (i = 0; i < count; i++)
+	n = i;
+	printf("%d", count);
+	printf("\n");
+	int m;
+	for (int j = 0; j < p; j++)
 	{
-
+	 m = n % ((int)pow(10,(count - 1)));
+	// printf("%d", m);
+		m = m*(pow(10,(count - (count-1))));
+		//printf("\n%d", m);
+		m = m + (n / pow(10,(count - 1)));
+		n = m;
 	}
+	printf("%d", n);
 
-
+	getch();
 	return 0;
 }
